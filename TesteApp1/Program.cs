116bdc8688585b7f8349b4e83f7b537695781dad
guijose03 <gujose00@gmail.com>
2023-03-15 15:43:03 -0300

    Testes antes da aula 2

--- TesteApp1/Program.cs
+++ TesteApp1/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Oie");
+            Console.WriteLine("Digite seu nome: ");
+
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Seu nome é "+nome);
+            Console.WriteLine("Você tem " + idade + " anos");
+
+           
         }
     }
 }
