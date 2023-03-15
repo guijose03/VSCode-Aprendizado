bfb7c48ea432614d0fce5eee3e35e1514b504022
guijose03 <gujose00@gmail.com>
2023-03-15 16:19:19 -0300

    Aula 2

--- TesteApp1/Program.cs
+++ TesteApp1/Program.cs
@@ -15,7 +15,9 @@
             Console.WriteLine("Seu nome é "+nome);
             Console.WriteLine("Você tem " + idade + " anos");
 
-           
+            double saldo = 10.897;
+
+            Console.WriteLine($"{nome} tem {idade} e um saldo bancario de {saldo:F2}");
         }
     }
 }
